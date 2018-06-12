489ffa4d6d1cb9a09ff3e8400800418a9294d47b
inger klekacz <ingernet@users.noreply.github.com>
2018-06-12 16:44:49 -0700

    EXTRA curly braces yayyyyyy puppet

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft (
   $url = 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
   $install_dir = '/opt/minecraft'
 ){
-  file {${install_dir}:
+  file {$install_dir:
     ensure => directory,    
   }
   file {"${install_dir}/minecraft_server.jar":
