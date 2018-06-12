f2a93a85e61d4726fe1821846a73e492c0dbdd23
inger klekacz <ingernet@users.noreply.github.com>
2018-06-12 11:37:01 -0700

    add pub key

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDC3n0Q/jFgivvxwSDZfqAuJZz1T3qdsv51s7e8dKhKlRjEagZgrIj0X0DfkJaxpuZrCabPfEKrnqS6okGZumQhhvN3Op7knPTkG6eMugL4A8I4o4y0E9LlWAEfPVzT/gsM5AVRAMECXhAIwtC682dNMUmrpVWFzD1ZZ9vMEvAWzDTni1/HIB3Y08s1TMyUYYpwTy5q2Md6H8SNlYk8+f3L2i74E5wBM33MVX1Fnr1FNzcicdceV5w7Aom+yo/hbAaMRJ/frIFlVDxlMHHaYBow/HIjctwF/ElVLzyVhOEyC+i1O1okbFLQPjpG9fsOij7xyCbou3buz1Zr83oCitTB',
 	}  
 }
